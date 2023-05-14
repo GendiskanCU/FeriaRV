e1dbd11f627162df52f2cc8882e7ce0dc33e5c28
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2023-05-15 00:01:02 +0200

    Movimiento con físicas

--- Assets/Scripts/Game03/DuckMovement.cs
+++ Assets/Scripts/Game03/DuckMovement.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class DuckMovement : MonoBehaviour
 {
-    [SerializeField] private float minPosY = 0.700f;
-    [SerializeField] private float maxPosY = 0.755f;
+    //[SerializeField] private float minPosY = 0.700f;
+    //[SerializeField] private float maxPosY = 0.755f;
     [SerializeField] private List<Transform> wayPoints;
 
-    [SerializeField][Range(0.008f, 0.1f)] private float floatingSpeed = 0.05f;
-    [SerializeField][Range(0.01f, 0.1f)] private float displacementSpeed = 0.02f;
+    //[SerializeField][Range(0.008f, 0.1f)] private float floatingSpeed = 0.05f;
+    [SerializeField][Range(0.1f, 0.5f)] private float displacementSpeed = 0.2f;
     [SerializeField][Range(-25f, 25f)] private float rotationSpeed = 10f;
+
+    [SerializeField][Range(0.3f, 1.0f)] private float timeForFloatPulse = 0.3f;
+    [SerializeField][Range(0.4f, 0.8f)] private float floatForce = 0.4f;
 
     private float initialPositionCorrection = 0.02f;
     private int temporalFloatingVariation = 1;    
@@ -18,16 +21,21 @@ public class DuckMovement : MonoBehaviour
     private byte nextWayPoint = 0;
     private float newRotationY;
 
+    private Rigidbody _rb;   
+
     
     // Start is called before the first frame update
     void Start()
-    {
+    {        
+        _rb = GetComponent<Rigidbody>();
+
+        /*//Alternativa sin tener en consideración las físicas
         float initialPositionY = Random.Range(minPosY + initialPositionCorrection, maxPosY - initialPositionCorrection);
-        transform.position = new Vector3(transform.position.x, initialPositionY, transform.position.z);
+        transform.position = new Vector3(transform.position.x, initialPositionY, transform.position.z);*/
         newRotationY = Random.Range(0, 360);
         transform.rotation = Quaternion.Euler(new Vector3(-90 ,newRotationY, 0));
-    }
-
+        StartCoroutine("Floating");
+    }  
    
 
     private void FixedUpdate() {
@@ -36,6 +44,7 @@ public class DuckMovement : MonoBehaviour
 
     private void MoveDuck()
     {        
+        /*//Alternativa de movimiento sin tener en consideración las físicas
         //Flotación
         transform.Translate(Vector3.up * (Time.fixedDeltaTime * floatingSpeed) * temporalFloatingVariation, Space.World);
         
@@ -59,6 +68,27 @@ public class DuckMovement : MonoBehaviour
 
         //Rotación
         newRotationY += rotationSpeed * Time.fixedDeltaTime;
-        transform.rotation = Quaternion.Euler(new Vector3(-90 ,newRotationY, 0));
+        transform.rotation = Quaternion.Euler(new Vector3(-90 ,newRotationY, 0));        */
+
+        //Desplazamiento
+        Vector3 direction = (wayPoints[nextWayPoint].position - transform.position).normalized;
+        _rb.MovePosition(transform.position + direction * displacementSpeed * Time.fixedDeltaTime);
+        if(Vector3.Distance(transform.position, wayPoints[nextWayPoint].position) <= distanceChangeWayPoint)
+            nextWayPoint++;
+        if(nextWayPoint >= wayPoints.Count)
+            nextWayPoint = 0;
+
+        //Rotación
+        newRotationY += rotationSpeed * Time.fixedDeltaTime;
+        transform.rotation = Quaternion.Euler(new Vector3(-90 ,newRotationY, 0));        
+    }
+
+    private IEnumerator Floating()
+    {
+        while(true)
+        {
+            yield return new WaitForSeconds(timeForFloatPulse);
+            _rb.AddForce(transform.forward * floatForce, ForceMode.Impulse);
+        }
     }
 }
