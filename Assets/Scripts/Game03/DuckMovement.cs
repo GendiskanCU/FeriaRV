5c3fec98830a72fa1413f7d90988c22962de6070
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2023-05-14 19:43:12 +0200

    Movimiento patos

--- Assets/Scripts/Game03/DuckMovement.cs
+++ Assets/Scripts/Game03/DuckMovement.cs
@@ -6,11 +6,17 @@ public class DuckMovement : MonoBehaviour
 {
     [SerializeField] private float minPosY = 0.700f;
     [SerializeField] private float maxPosY = 0.755f;
+    [SerializeField] private List<Transform> wayPoints;
 
-    [SerializeField] private float floatingSpeed = 0.05f;
+    [SerializeField][Range(0.008f, 0.1f)] private float floatingSpeed = 0.05f;
+    [SerializeField][Range(0.01f, 0.1f)] private float displacementSpeed = 0.02f;
+    [SerializeField][Range(-25f, 25f)] private float rotationSpeed = 10f;
 
     private float initialPositionCorrection = 0.02f;
-    private int temporalFloatingVariation = 1;
+    private int temporalFloatingVariation = 1;    
+    private float distanceChangeWayPoint = 0.05f;
+    private byte nextWayPoint = 0;
+    private float newRotationY;
 
     
     // Start is called before the first frame update
@@ -18,17 +24,19 @@ public class DuckMovement : MonoBehaviour
     {
         float initialPositionY = Random.Range(minPosY + initialPositionCorrection, maxPosY - initialPositionCorrection);
         transform.position = new Vector3(transform.position.x, initialPositionY, transform.position.z);
+        newRotationY = Random.Range(0, 360);
+        transform.rotation = Quaternion.Euler(new Vector3(-90 ,newRotationY, 0));
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+   
+
+    private void FixedUpdate() {
         MoveDuck();
     }
 
     private void MoveDuck()
     {        
-        
+        //Flotación
         transform.Translate(Vector3.up * (Time.fixedDeltaTime * floatingSpeed) * temporalFloatingVariation, Space.World);
         
         float currentPositionY = transform.position.y;
@@ -40,5 +48,17 @@ public class DuckMovement : MonoBehaviour
         {
             temporalFloatingVariation = 1;
         }
+
+        //Desplazamiento
+        transform.position = Vector3.MoveTowards(transform.position, wayPoints[nextWayPoint].position,
+            displacementSpeed * Time.fixedDeltaTime);
+        if(Vector3.Distance(transform.position, wayPoints[nextWayPoint].position) <= distanceChangeWayPoint)
+            nextWayPoint++;
+        if(nextWayPoint >= wayPoints.Count)
+            nextWayPoint = 0;
+
+        //Rotación
+        newRotationY += rotationSpeed * Time.fixedDeltaTime;
+        transform.rotation = Quaternion.Euler(new Vector3(-90 ,newRotationY, 0));
     }
 }
