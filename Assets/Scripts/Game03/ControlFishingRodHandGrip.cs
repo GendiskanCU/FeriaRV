57b5caf3697eadccfa418fd71217698de3264543
Gendiskan-CU <97439241+GendiskanCU@users.noreply.github.com>
2023-05-15 21:22:07 +0200

    Caña de pescar

---
+++ Assets/Scripts/Game03/ControlFishingRodHandGrip.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlFishingRodHandGrip : MonoBehaviour
+{
+    [SerializeField] private Transform transformForLeftHand;
+    [SerializeField] private Transform transformForRightHand;
+    // Start is called before the first frame update
+
+    private OVRGrabbable _ovrGrabbable;
+
+
+    private void Start() {
+        _ovrGrabbable = GetComponent<OVRGrabbable>();
+    }
+
+    private void OnTriggerEnter(Collider other) {
+
+        if(other.CompareTag("PlayerHand"))
+        {
+            OVRGrabber grabber = other.GetComponent<OVRGrabber>();
+            if(grabber.GetController() == OVRInput.Controller.LTouch)
+            {
+                _ovrGrabbable.SetSnapOffset(transformForLeftHand);
+                GetComponent<Shoot>().LeftHand = true;
+            }
+
+            if(grabber.GetController() == OVRInput.Controller.RTouch)
+            {
+                _ovrGrabbable.SetSnapOffset(transformForRightHand);
+                GetComponent<Shoot>().LeftHand = false;
+            }
+        }        
+    }  
+}
